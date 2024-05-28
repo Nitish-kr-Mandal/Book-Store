62d6fc2370ac20d2fb88e85af209d9462ab055ec
Nitish-kr-Mandal <nitishm8292@gmail.com>
2024-05-28 11:55:15 +0530

    fix navbar and courses bug

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -8,10 +8,11 @@ function Course() {
   return (
     <>
     <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-        <div className=' mt-28 items-center justify-center text-center'>
-            <h1 className=' text-2xl font-semibold md:text-4xl'>We are delighted to have you
+        <div className='items-center justify-center text-center'>
+            <h1 className=' pt-28 text-2xl font-semibold md:text-4xl'>We are delighted to have you
              <span className=' text-pink-600'> Here</span></h1>
-             <p>Welcome! We are thrilled to have you here and excited to share our passion for books with you. Our store is a haven for book lovers, offering a vast selection of genres and titles to satisfy every reader's curiosity and taste. Our dedicated team is here to assist you in finding the perfect book, providing personalized recommendations, and ensuring that your visit is enjoyable and memorable. Thank you for choosing our book store.</p>
+             
+             <p className='pt-4'>Welcome! We are thrilled to have you here and excited to share our passion for books with you. Our store is a haven for book lovers, offering a vast selection of genres and titles to satisfy every reader's curiosity and taste. Our dedicated team is here to assist you in finding the perfect book, providing personalized recommendations, and ensuring that your visit is enjoyable and memorable. Thank you for choosing our book store.</p>
              
              <Link to="/">
              <button className=' mt-6 bg-pink-500 text-white px-4 py-2 hover:bg-pink-700 duration-300 cursor-pointer rounded-md'>Back</button>
