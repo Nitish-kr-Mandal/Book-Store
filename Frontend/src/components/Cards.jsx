c101bd9a9728beea7c46b3cb709dda6f732b3c27
Nitish-kr-Mandal <nitishm8292@gmail.com>
2024-05-28 11:21:54 +0530

    add theme control

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -4,7 +4,7 @@ function Cards({item}) {
   return (
     <>
     <div className='my-5 p-3'>
-    <div className="card w-92 bg-base-100 shadow-xl hover:scale-110 duration-200">
+    <div className="card w-92 bg-base-100 shadow-xl hover:scale-110 duration-200 dark:bg-slate-900 dark:text-white dark:border">
   <figure><img src={item.image} alt="Book" /></figure>
   <div className="card-body">
     <h2 className="card-title">
