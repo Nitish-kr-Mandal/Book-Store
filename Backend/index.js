bd8893194139c359db30900906abdaca4aa540d1
Nitish-kr-Mandal <nitishm8292@gmail.com>
2024-05-31 17:00:01 +0530

    add user controller, user routes and signup

--- Backend/index.js
+++ Backend/index.js
@@ -5,10 +5,12 @@ import cors from "cors";
 
 
 import bookRoute from "./route/book.route.js"
+import userRoute from "./route/user.route.js"
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 dotenv.config();
 
@@ -24,7 +26,8 @@ try {
 }
 
 // defining routes
-app.use("/book",bookRoute)
+app.use("/book",bookRoute);
+app.use("/user",userRoute);
 
 
 app.listen(PORT, () => {
